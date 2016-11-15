a21783f86b428586250b3fba2479c252b0d57c07
Danilo Gómez Gómez <danilogomez3.14@gmail.com>
2016-11-15 18:19:10 -0500

    Add `putc` routine as base for coloring.

--- src/video.asm
+++ src/video.asm
@@ -3,6 +3,17 @@
 ; Frame buffer location
 %define FBUFFER 0xB8000
 
+; FBOFFSET(byte row, byte column)
+%macro FBOFFSET 2.nolist
+    xor eax, eax
+    mov al, COLS
+    mul byte %1
+    add al, %2
+    adc ah, 0
+    shl ax, 1
+%endmacro
+
+
 section .text
 
 ; clear(byte char, byte attrs)
@@ -15,3 +26,14 @@ clear:
   cld
   rep stosw
   ret
+
+
+; putc(char chr, byte color, byte r, byte c)
+;      4         5           6       7
+global putc
+putc:
+    ; calc famebuffer offset 2 * (r * COLS + c)
+    FBOFFSET [esp + 6], [esp + 7]
+
+    mov bx, [esp + 4]
+    mov [FBUFFER + eax], bx
